6dc2f9d2c7e414b935f343bb3b37f457bdeb66c0
Benjamin Choquet <benjamin.choquet@heliopsis.net>
2013-09-27 17:29:09 +0200

    Fix EZP-21548: Clear role assignments cache when dealing with locations
    As UserService relies on LocationService to assign/unassign groups to users,
    we must find a way to invalidate role assignments cache when creating, deleting
    and moving locations.

--- eZ/Publish/Core/Persistence/Cache/Tests/LocationHandlerTest.php
+++ eZ/Publish/Core/Persistence/Cache/Tests/LocationHandlerTest.php
@@ -462,9 +462,15 @@ class LocationHandlerTest extends HandlerTest
     {
         $this->loggerMock->expects( $this->once() )->method( 'logCall' );
         $this->cacheMock
-            ->expects( $this->once() )
+            ->expects( $this->at( 0 ) )
             ->method( 'clear' )
             ->with( 'location' )
+            ->will( $this->returnValue( true ) );
+
+        $this->cacheMock
+            ->expects( $this->at( 1 ) )
+            ->method( 'clear' )
+            ->with( 'user', 'role', 'assignments', 'byGroup' )
             ->will( $this->returnValue( true ) );
 
         $innerHandlerMock = $this->getMock( 'eZ\\Publish\\SPI\\Persistence\\Content\\Location\\Handler' );
@@ -588,6 +594,12 @@ class LocationHandlerTest extends HandlerTest
             ->with( 'content', 'locations' )
             ->will( $this->returnValue( true ) );
 
+        $this->cacheMock
+            ->expects( $this->at( 4 ) )
+            ->method( 'clear' )
+            ->with( 'user', 'role', 'assignments', 'byGroup' )
+            ->will( $this->returnValue( true ) );
+
         $innerHandlerMock = $this->getMock( 'eZ\\Publish\\SPI\\Persistence\\Content\\Location\\Handler' );
         $this->persistenceFactoryMock
             ->expects( $this->once() )
@@ -676,6 +688,18 @@ class LocationHandlerTest extends HandlerTest
             ->with( 'content', 'locations', 2 )
             ->will( $this->returnValue( true ) );
 
+        $this->cacheMock
+            ->expects( $this->at( 3 ) )
+            ->method( 'clear' )
+            ->with( 'user', 'role', 'assignments', 'byGroup', 2 )
+            ->will( $this->returnValue( true ) );
+
+        $this->cacheMock
+            ->expects( $this->at( 4 ) )
+            ->method( 'clear' )
+            ->with( 'user', 'role', 'assignments', 'byGroup', 'inherited/' . 2 )
+            ->will( $this->returnValue( true ) );
+
         $handler = $this->persistenceHandler->locationHandler();
         $handler->create( new CreateStruct );
     }
@@ -696,6 +720,12 @@ class LocationHandlerTest extends HandlerTest
             ->expects( $this->at( 1 ) )
             ->method( 'clear' )
             ->with( 'content' )
+            ->will( $this->returnValue( true ) );
+
+        $this->cacheMock
+            ->expects( $this->at( 2 ) )
+            ->method( 'clear' )
+            ->with( 'user', 'role', 'assignments', 'byGroup' )
             ->will( $this->returnValue( true ) );
 
         $innerHandlerMock = $this->getMock( 'eZ\\Publish\\SPI\\Persistence\\Content\\Location\\Handler' );
